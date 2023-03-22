53b0d3b63af73ffd2b12eabf6e261343dfe21e25
root <jakmbugua57@gmail.com>
2023-03-22 04:01:14 -0700

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,7 +1,10 @@
-#include <unistd.h>
-#include "main.h"
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
-
+#include <stdio.h> 
+ 
+int write(int filedes, const char *buf, unsigned int nbyte); 
+int main( int argc, char** argv ) 
+{ 
+   write(1, "_putchar\n", 13); 
+ 
+   return 0; 
+} 
+ 
